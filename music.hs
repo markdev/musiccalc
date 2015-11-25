fb043a3a3e0cee146bf2d68838d041ee907400f5
Mark Karavan <mark.karavan@gmail.com>
2015-11-24 22:31:53 -0500

    adds the ability to get scales

--- music.hs
+++ music.hs
@@ -15,6 +15,7 @@ instance Show Note where
     show AsBb = "A#/Bb"
     show B = "B"
 data Diatonic = Unison | Min2 | Maj2 | Min3 | Maj3 | Per4 | Tritone | Per5 | Min6 | Maj6 | Min7 | Maj7 | Octave deriving (Read, Show, Eq, Ord, Enum, Bounded)
+data Scale = Major | Minor deriving (Read, Show, Eq)
 
 listOfNotes :: Note -> [Note]
 listOfNotes root = take 13 $ dropWhile (/=root) $ cycle noteList
@@ -29,5 +30,15 @@ mapDiatonics root = zip listOfDiatonics $ listOfNotes root
 
 getDiatonicOf :: Note -> Diatonic -> Note
 getDiatonicOf root diat = foldl (\acc x -> if fst x == diat then snd x else acc) root $ mapDiatonics root
+
+getScale :: Note -> Scale -> [(Diatonic, Note)]
+getScale note scale = filter (\ x -> (fst x) `elem` (listOfNotesIn scale)) $ mapDiatonics note 
+
+listOfNotesIn :: Scale -> [Diatonic]
+listOfNotesIn scale
+   | scale == Major = [Unison, Maj2, Maj3, Per4, Per5, Maj6, Maj7, Octave]
+   | scale == Minor = [Unison, Maj2, Min3, Per4, Per5, Maj6, Maj7, Octave]
+   | otherwise      = [Unison, Maj2, Min3, Per4, Per5, Maj6, Maj7, Octave]
+
 
 
