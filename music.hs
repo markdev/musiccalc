b266f87158c8dbea1935277c16ba37120d0b0aaf
Mark Karavan <mark.karavan@gmail.com>
2015-11-24 23:05:37 -0500

    adds more scales

--- music.hs
+++ music.hs
@@ -15,7 +15,7 @@ instance Show Note where
     show AsBb = "A#/Bb"
     show B = "B"
 data Diatonic = Unison | Min2 | Maj2 | Min3 | Maj3 | Per4 | Tritone | Per5 | Min6 | Maj6 | Min7 | Maj7 | Octave deriving (Read, Show, Eq, Ord, Enum, Bounded)
-data Scale = Major | Minor deriving (Read, Show, Eq)
+data Scale = Major | NatMin | HarmMin | MajPent | MinPent deriving (Read, Show, Eq)
 
 listOfNotes :: Note -> [Note]
 listOfNotes root = take 13 $ dropWhile (/=root) $ cycle noteList
@@ -32,13 +32,22 @@ getDiatonicOf :: Note -> Diatonic -> Note
 getDiatonicOf root diat = foldl (\acc x -> if fst x == diat then snd x else acc) root $ mapDiatonics root
 
 getScale :: Note -> Scale -> [(Diatonic, Note)]
-getScale note scale = filter (\ x -> (fst x) `elem` (listOfNotesIn scale)) $ mapDiatonics note 
+getScale note scale = filter (\ x -> (fst x) `elem` (getIntervals scale)) $ mapDiatonics note
 
-listOfNotesIn :: Scale -> [Diatonic]
-listOfNotesIn scale
-   | scale == Major = [Unison, Maj2, Maj3, Per4, Per5, Maj6, Maj7, Octave]
-   | scale == Minor = [Unison, Maj2, Min3, Per4, Per5, Maj6, Maj7, Octave]
-   | otherwise      = [Unison, Maj2, Min3, Per4, Per5, Maj6, Maj7, Octave]
+justNotes :: [(Diatonic, Note)] -> [Note]
+justNotes = foldl (\ acc x -> acc ++ [snd x]) []
+
+justDiatonics :: [(Diatonic, Note)] -> [Diatonic]
+justDiatonics = foldl (\ acc x -> acc ++ [fst x]) []
+
+getIntervals :: Scale -> [Diatonic]
+getIntervals scale
+   | scale == Major     = [Unison,       Maj2,       Maj3, Per4,        Per5,       Maj6,      Maj7, Octave]
+   | scale == NatMin    = [Unison,       Maj2, Min3,       Per4,        Per5, Min6,       Min7,      Octave]
+   | scale == HarmMin   = [Unison,       Maj2, Min3,       Per4,        Per5, Min6,            Maj7, Octave]
+   | scale == MajPent   = [Unison,       Maj2,       Maj3,              Per5,       Maj6,            Octave]
+   | scale == MinPent   = [Unison,             Min3,       Per4,        Per5,             Min7,      Octave]
+
 
 
 
